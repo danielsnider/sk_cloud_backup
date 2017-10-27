a82b2968edaaea045e3744e7313b16770d07151d
Daniel Snider <danielsnider12@gmail.com>
2017-10-27 15:40:41 -0400

    intcremental backup

--- loop_clone_Operetta.ps1
+++ loop_clone_Operetta.ps1
@@ -1,10 +1,24 @@
 net use z: \\carbon.research.sickkids.ca\rkafri
 $base = "OPRETTA\Operetta Raw Data\Mammalian cells"
 $log_dir = "Z:\DanielS\Projects\Cloud Backup\logs"
-Remove-Item -path $log_dir\*.log
+
+# Get log file list of already done folder backups
+$done_folders = Get-ChildItem "$log_dir" | sort LastWriteTime -Descending
+
+# Delete log file of the most recent done folder backup incase it is didn't finish
+Remove-Item -path $done_folders[0].FullName
+
 Get-ChildItem "Z:\$base" | ?{ $_.PSIsContainer } | Where{$_.LastWriteTime -gt (Get-Date).AddDays(-265)} |
 Foreach-Object {
+    
     $target = $_.Name
+    
+    # Skip folders that are done. ie. of the done folder names, if any match the current target, skip
+    if($done_folders.FullName -match $target) {
+      echo "Skipping target: $target"
+      return
+    }
+
     echo "Rcloning target: $target"
     $date_full = (Get-Date)
     $date = (Get-Date).ToString('yyyyMMdd')
